e267c0d68056195176a68644aec403c3e67b111c
rubydiarmit <34691001+rubydiarmit@users.noreply.github.com>
2018-01-16 13:39:10 -0500

    Add files via upload

---
+++ EvenOrOdd.py
@@ -0,0 +1,7 @@
+#print even or odd in a range of numbers
+for number in range(50):
+    if number%2==0:
+        print("Even Number")
+    elif number %2==1:
+        print("Odd Number")
+    print(number)
